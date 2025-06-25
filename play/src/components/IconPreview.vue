66965ba8c6bdede04ffce5d677cd0fe69c4b9b3c
nik26788 <risinnshow@gmail.com>
2025-06-25 11:11:14 +0800

    chore(play): re-organizing components

---
+++ play/src/components/IconPreview.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { ArrowDownSharp } from '@vicons/ionicons5'
+</script>
+
+<template>
+    <h2>Icon</h2>
+    <div>
+        <cz-icon :color="'purple'" size="20">
+            <ArrowDownSharp />
+        </cz-icon>
+        <cz-icon :color="'tomato'" size="20">
+            <ArrowDownSharp />
+        </cz-icon>
+        <cz-icon :color="'green'" size="20">
+            <ArrowDownSharp />
+        </cz-icon>
+        <cz-icon :color="'orange'" size="20">
+            <ArrowDownSharp />
+        </cz-icon>
+    </div>
+</template>
